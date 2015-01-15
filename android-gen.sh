f07010a8ff4938d8afe16149a407bc5b70a096ca
Jennal <jennalcn@gmail.com>
2015-01-15 14:06:19 +0800

    build android without ssl

--- android-gen.sh
+++ android-gen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export ANDROID_TOOLCHAIN_DIR=$PWD/android-toolchain
+export PATH=$NDK_ROOT/platforms/android-9/arch-arm/usr/include:$PATH
 export PATH=$ANDROID_TOOLCHAIN_DIR/bin:$PATH
 export AR=arm-linux-androideabi-ar
 export CC=arm-linux-androideabi-gcc
@@ -7,4 +9,4 @@ export CXX=arm-linux-androideabi-g++
 export LINK=arm-linux-androideabi-g++
 export PLATFORM=android
 
-gyp --depth=. -Dtarget_arch=arm -DOS=android -Dpomelo_library=static_library -Duse_sys_openssl=false -Dbuild_jpomelo=true
+gyp --depth=. -Dtarget_arch=arm -DOS=android -Dpomelo_library=static_library -Duse_sys_openssl=false -Dbuild_jpomelo=true --format=make
---
+++ deps/jansson/jansson2/jansson2.m
@@ -0,0 +1,13 @@
+//
+//  jansson2.m
+//  jansson2
+//
+//  Created by zhengle on 14-12-25.
+//
+//
+
+#import "jansson2.h"
+
+@implementation jansson2
+
+@end
